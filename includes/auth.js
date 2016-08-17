d7fa8d62d24e5ea628cd216a2ddde24e402e3d2a
Joseph Schilz <joseph@schilz.org>
2016-08-17 14:55:33 -0700

    Facilitate login.

--- includes/auth.js
+++ includes/auth.js
@@ -21,8 +21,12 @@ MagicFAQ.auth = (function () {
     /**
      * @returns {undefined}
      */
-    var renewToken = function()
+    var renewToken = function(tryAgain)
     {
+        if (typeof tryAgain === 'undefined') {
+            tryAgain = false;
+        }
+
         $.get(
             {
                 url: MagicFAQ.baseURL + 'auth/admin/',
@@ -45,23 +49,48 @@ MagicFAQ.auth = (function () {
 
                         console.log(loginURI);
 
-                        authWindow = window.open(loginURI, '', 'width=800,height=600');
+                        var authWindow = window.open(loginURI, '', 'width=800,height=600');
 
-                        MagicFAQ.notify(
-                            'Need to re-authenticate. Please sign in and then try again. Disable pop-up blockers if sign in window does not appear.',
-                            'info',
-                            10000
-                        );
+                        if (tryAgain) {
+                            MagicFAQ.notify(
+                                'You need to re-authenticate. Please sign in and then try again. Disable pop-up blockers if sign in window does not appear.',
+                                'info',
+                                10000
+                            );
+                            
+                            var timer = setInterval(function() {
+                                if(authWindow.closed) {
+                                    clearInterval(timer);
+                                    renewToken(false);
 
-                        authWindow.onclose = function() {
-                            if (typeof getToken() !== 'undefined') {
-                                MagicFAQ.notify(
-                                    'You have been re-authenticated. Please try your operation again.',
-                                    'success',
-                                    10000
-                                );
-                            }
+                                    if (tokenIsValid()) {
+                                        MagicFAQ.notify(
+                                            'You have been re-authenticated. Please try your operation again.',
+                                            'success',
+                                            10000
+                                        );
+                                    } else {
+                                        MagicFAQ.notify(
+                                            'Unanticipated authentication error. Try refreshing the page.',
+                                            'error',
+                                            10000
+                                        );
+                                    }
+                                }
+                            }, 1000);
                         }
+
+
+                        // authWindow.onclose = function() {
+                        //     console.log("HEREEEEE!!!");
+                        //     if (typeof getToken() !== 'undefined') {
+                        //         MagicFAQ.notify(
+                        //             'You have been re-authenticated. Please try your operation again.',
+                        //             'success',
+                        //             10000
+                        //         );
+                        //     }
+                        // }
                     }
                 },
                 timeout: 3000,
@@ -76,7 +105,7 @@ MagicFAQ.auth = (function () {
     var getToken = function()
     {
         if (!tokenIsValid()) {
-            renewToken()
+            renewToken(true)
         }
         
         return token;
